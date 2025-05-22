07cbcb30c2319a7f22ee00fd7224ce179d1dbf9b
Raj Patel <raj721patel@gmail.com>
2025-05-22 17:52:01 -0500

    Added other shadcn ui components and created/edited button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,35 @@
 import { Button } from "@/components/ui/button"
-import { TestComponent } from "@/features/test"
 
 export default function Home() {
   return (
-    <div>
-      <Button variant='destructive' size='sm'> Click Me</Button>
-      <p className="text-red-500 font-semibold">
-        Aero
-      </p>
+    <div className="">
+      <Button size="lg">
+        Primary
+      </Button>
 
-      <TestComponent></TestComponent>
+      <Button variant="secondary">
+        Secondary
+      </Button>
+
+      <Button variant="destructive">
+        Destructive
+      </Button>
+
+      <Button variant="ghost">
+        Ghost
+      </Button>
+
+      <Button variant="muted">
+        Muted
+      </Button>
+
+      <Button variant="outline">
+        Outline
+      </Button>
+
+      <Button variant="tertiary">
+        Tertiary
+      </Button>
     </div>
   )
 }
