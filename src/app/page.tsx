6837f3f470e5d265a9689e8a0e18c759e0c269b8
Raj Patel <raj721patel@gmail.com>
2025-05-28 13:42:55 -0500

    Creating session middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,29 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if(!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data, isLoading, router]);
+
   return (
-    <div className="">
-
-      <Input />
-
-      <Button>
-        Primary
-      </Button>
-
-      <Button variant="secondary">
-        Secondary
-      </Button>
-
-      <Button variant="destructive">
-        Destructive
-      </Button>
-
-      <Button variant="ghost">
-        Ghost
-      </Button>
-
-      <Button variant="muted">
-        Muted
-      </Button>
-
-      <Button variant="outline">
-        Outline
-      </Button>
-
-      <Button variant="tertiary">
-        Tertiary
+    <div>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>
+        Logout
       </Button>
     </div>
-  )
-}
+  );
+};
--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -7,8 +7,18 @@ import { loginSchema, registerSchema } from "../schemas";
 import { AUTH_COOKIE } from "../constants";
 
 import { createAdminClient } from "@/lib/appwrite";
+import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+  .get(
+    "/current",
+    sessionMiddleware,
+    (c) => {
+      const user = c.get("user");
+
+      return c.json({ data: user });
+    }
+  )
   .post(
     "/login",
     zValidator("json", loginSchema),
@@ -63,8 +73,11 @@ const app = new Hono()
       return c.json({ success: true });
     }
   )
-  .post("/logout", (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
+    const account = c.get("account");
+
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
 
     return c.json({ success: true });
   });
