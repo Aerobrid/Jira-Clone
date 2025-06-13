ac543aa97a983535c130caa02816e6ca3187263f
Raj Patel <raj721patel@gmail.com>
2025-06-12 21:44:34 -0500

    api folder notes

--- src/features/auth/api/use-current.ts
+++ src/features/auth/api/use-current.ts
@@ -1,17 +1,27 @@
+// import useQuery hook from react-query to automatically fetch and cache data
 import { useQuery } from "@tanstack/react-query";
 
+// Importing the client from the RPC library to make API calls
 import { client } from "@/lib/rpc";
 
+// exporting custom hook useCurrent 
+// This hook is used to fetch the current authenticated user's data with ease
 export const useCurrent = () => {
+    // use useQuery to manage fetching, caching, and updating the current user data
     const query = useQuery({
+        // pass in a unique key for the query describing the data being fetched
         queryKey: ["current"],
+        // function to actually fetch the data, this function is called when the query is executed
         queryFn: async () => {
+            // make get request to the API endpoint to fetch current user data
             const response = await client.api.auth.current.$get();
 
+            // if the response is not ok, return null
             if (!response.ok) {
                 return null;
             }
 
+            // parse the JSON response to get the current user data
             const { data } = await response.json();
 
             return data;
--- src/features/auth/api/use-login.ts
+++ src/features/auth/api/use-login.ts
@@ -1,19 +1,30 @@
+// useMutation hook for user login
+// we can use this hook since we wrapped the app with the QueryProvider in layout.tsx
 import { useMutation } from "@tanstack/react-query";
+// hono helpers to figure out request and response types for login API call
 import { InferRequestType, InferResponseType } from "hono";
 
+// to make API calls, we import the client from our RPC library
 import { client } from "@/lib/rpc";
 
+// Define types for the response and request of the login API call which is a POST request
 type ResponseType = InferResponseType<typeof client.api.auth.login["$post"]>;
 type RequestType = InferRequestType<typeof client.api.auth.login["$post"]>;
 
+// Custom hook to handle user login
 export const useLogin = () => {
+  // useMutation helps run server-side mutations and manage their state
+  // <> tells TS what types to expect for the response, error, and request
   const mutation = useMutation<
     ResponseType,
     Error,
     RequestType
   >({
+    // The mutation function that will be called when the mutation is triggered
     mutationFn: async ({ json }) => {
+      // takes json data from the request and makes a POST request to the login endpoint
       const response = await client.api.auth.login["$post"]({ json });
+      // returns the parsed JSON response from the API
       return  await response.json();
     },
   });
